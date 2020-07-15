3e2a09fb465b2166d7f8008dea28b43c0339cd3a
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-15 17:26:19 -0400

    write new user reducers

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,19 +1,26 @@
-import { UserActionTypes } from './user.types';
+import UserActionTypes from './user.types';
 
 const INITIAL_STATE = {
-    currentUser: null
+    currentUser: null,
+    error: null
 }
 
 const userReducer = (state = INITIAL_STATE, action) => {
-switch (action.type) {
-    case UserActionTypes.SET_CURRENT_USER:
-        return {
-            ...state,
-            currentUser: action.payload
-        }
-    default:
-        return state;
-}
+    switch (action.type) {
+        case UserActionTypes.SIGN_IN_SUCCESS:
+            return {
+                ...state,
+                currentUser: action.payload,
+                error: null
+            }
+        case UserActionTypes.SIGN_IN_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
 };
 
 export default userReducer;
