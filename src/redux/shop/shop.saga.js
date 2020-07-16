e90e321121de3cf7c5ce0bb4c2a4fec4bfb0ad00
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-16 12:36:38 -0400

    export shopSagas function added

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { firestore, convertCollectionSnapshotToMap } from '../../firebase/firebase.utils';
 import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
 
@@ -23,4 +23,8 @@ export function* fetchCollectionsAsync() {
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START,
         fetchCollectionsAsync)
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)])
 }
