36229d478453803ac559f26abf1ee726e7d7c3d8
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-16 12:34:50 -0400

    work on user actions fro redux.saga

--- src/redux/user/user.saga.js
+++ src/redux/user/user.saga.js
@@ -1,14 +1,15 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
 import UserActionTypes from './user.types';
-import { signInSuccess, signInFailure } from './users.actions';
+import { signInSuccess, signInFailure, signOutSuccess, signOutFailure, signUpSuccess, signUpFailure } from './users.actions';
 
-import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+import { auth, googleProvider, createUserProfileDocument, getCurrentUser } from '../../firebase/firebase.utils';
 
 // listen to GoogleSingInStart and then trigger actual one from the saga
+
 // write reusable generator function
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
     try {
-        const userRef = yield call(createUserProfileDocument, userAuth)
+        const userRef = yield call(createUserProfileDocument, userAuth, additionalData)
         const userSnapshot = yield userRef.get();
         yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
         // console.log(user)
@@ -33,11 +34,10 @@ export function* onGoogleSignInStart() {
 
 export function* signInWithEmail({ payload: { email, password } }) {
     try {
+        // destructure user from the object we get
         const { user } = yield auth.signInWithEmailAndPassword(email, password);
-        console.log(user);
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get();
-        yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+        // console.log(user);
+        yield getSnapshotFromUserAuth(user);
     } catch (error) {
         put(signInFailure(error))
     }
@@ -47,6 +47,66 @@ export function* onEmailSignInStart() {
     yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail)
 }
 
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield getCurrentUser();
+        if (!userAuth) return;
+        yield getSnapshotFromUserAuth(userAuth);
+    } catch (error) {
+        yield put(signInFailure(error))
+    }
+}
+
+export function* onCheckUserSession() {
+    yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated)
+}
+
+// SIGN OUT 
+export function* signOut() {
+    try {
+        yield auth.signOut()
+        yield put(signOutSuccess())
+    } catch (error) {
+        yield put(signOutFailure(error))
+    }
+}
+
+export function* onSignOutStart() {
+    yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
+}
+
+// SIGN UP
+
+export function* signUp({ payload: { email, password, displayName } }) {
+    try {
+        const { user } = yield auth.createUserWithEmailAndPassword(
+            email,
+            password
+        );
+        yield put(signUpSuccess({ user, additionalData: { displayName } }))
+    } catch (error) {
+        yield put(signUpFailure(error))
+    }
+}
+
+export function* onSignUpStart() {
+    yield takeLatest(UserActionTypes.SIGN_UP_START, signUp)
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+    yield getSnapshotFromUserAuth(user, additionalData);
+}
+
+export function* onSignUpSuccess() {
+    yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp)
+}
+
 export function* userSagas() {
-    yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+    yield all(
+        [call(onGoogleSignInStart),
+        call(onEmailSignInStart),
+        call(onCheckUserSession),
+        call(onSignOutStart),
+        call(onSignUpStart),
+        call(onSignUpSuccess)]);
 }
--- src/redux/user/users.actions.js
+++ src/redux/user/users.actions.js
@@ -20,3 +20,34 @@ export const emailSignInStart = (emailAndPassword) => ({
     payload: emailAndPassword
 });
 
+export const checkUserSession = () => ({
+    type: UserActionTypes.CHECK_USER_SESSION
+})
+
+export const signOutStart = () => ({
+    type: UserActionTypes.SIGN_OUT_START
+})
+
+export const signOutSuccess = () => ({
+    type: UserActionTypes.SIGN_OUT_SUCCESS
+})
+
+export const signOutFailure = (error) => ({
+    type: UserActionTypes.SIGN_IN_FAILURE,
+    payload: error
+})
+
+export const signUpStart = (userCredentials) => ({
+    type: UserActionTypes.SIGN_UP_START,
+    payload: userCredentials
+})
+
+export const signUpSuccess = ({ user, additionalData }) => ({
+    type: UserActionTypes.SIGN_UP_SUCCESS,
+    payload: { user, additionalData }
+})
+
+export const signUpFailure = (error) => ({
+    type: UserActionTypes.SIGN_UP_FAILURE,
+    payload: error
+})
