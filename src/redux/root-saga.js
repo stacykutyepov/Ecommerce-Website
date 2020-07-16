51a3b057fae45194e48b67d1ba48da3946622573
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-07-16 12:36:07 -0400

    add all sagas to root sagas

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,9 +1,10 @@
 import { all, call } from 'redux-saga/effects';
 
-import { fetchCollectionsStart } from './shop/shop.saga';
+import { shopSagas } from './shop/shop.saga';
 import { userSagas } from './user/user.saga';
+import { cartSagas } from './cart/cart.sagas';
 
 export default function* rootSaga() {
     // all - allows to take different sagas concurrently
-    yield all([call(fetchCollectionsStart), call(userSagas)]);
+    yield all([call(shopSagas), call(userSagas), call(cartSagas)]);
 }
